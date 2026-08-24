fc0d36bf02a8d9ca77963c58d9ad7ad39f1cad3a
Timo Meyer <timomeyer@Mini-von-Timo.fritz.box>
2026-08-24 21:37:38 +0200

    fixed loading

--- ingestion/load_raw.py
+++ ingestion/load_raw.py
@@ -16,12 +16,14 @@ XLSX_FILE_PATH = "MHLV_data-2015-2019.xlsx"
 
 
 def load_2019_2026_file(path: str):
-    df: DataFrame = pl.read_csv(path, separator="\t")
+    df: DataFrame = pl.read_csv(f"data/raw/{path}", separator="\t")
+    if "CountryCode" not in df.columns:  # 2021/2022 sind mit ; getrennt
+        df = pl.read_csv(f"data/raw/{path}", separator=";")
     return df
 
 
 def load_2015_2019_file(path: str):
-    sheets = pl.read_excel(path, sheet_id=None)
+    sheets = pl.read_excel(f"data/raw/{path}", sheet_name=["2015-2017", "2018-2019"])
     df1 = sheets["2015-2017"]
     df2 = sheets["2018-2019"]
     df = pl.concat([df1, df2], how="vertical")
@@ -58,3 +60,7 @@ def create_panel():
         dataframes.append(df)
 
     df_gesamt = pl.concat(dataframes, how="vertical")
+    save_df(df=df_gesamt)
+
+
+create_panel()
